8c9df2b393194e0bbdd19d10bb603330b8b91c20
Shubham Mishra <shubhammishra2751@gmail.com>
2021-05-19 16:45:42 +0530

    Fouth commit

--- controller/connection.php
+++ controller/connection.php
@@ -1,13 +1,13 @@
 <?php
     function connect(){
-        // $dbHost= "localhost";
-        // // // here mysql port is 3306
-        // $dbHost = "localhost";
-        // $user= "root";
-        // $pass= "";
-        // $dbname="inventory-management";
+    //     $dbHost= "localhost";
+    //     // // // here mysql port is 3306
+    //     $dbHost = "localhost";
+    //     $user= "root";
+    //    $pass= "";
+    //     $dbname="inventory-management";
 
-        $dbHost = "remotemysql.com";
+       $dbHost = "remotemysql.com";
         $user= "Q8jhOPHLc5";
         $pass= "LSHr0CHDvC";
        	$dbname="Q8jhOPHLc5";
